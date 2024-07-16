66dcfd263684aa231173c44910b875b8cd4fd953
lklmvch <aleska.klimovich@gmail.com>
2024-07-16 19:42:41 +0300

    1287. Element Appearing More Than 25% In Sorted Array

---
+++ frequency.py
@@ -0,0 +1,14 @@
+'''Given an integer array sorted in non-decreasing order, there is exactly one integer in the array
+that occurs more than 25% of the time, return that integer.'''
+
+class Solution(object):
+    def findSpecialInteger(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        percent = len(arr) / 4
+
+        for i in arr:
+            if arr.count(i) > percent:
+                return i
