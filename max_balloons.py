df4cedc9b12bd7560cc1749a2bee86a4a612ecf2
lklmvch <aleska.klimovich@gmail.com>
2024-07-24 20:34:09 +0300

    Removed files.

--- max_balloons.py
+++ max_balloons.py
@@ -3,18 +3,17 @@
 You can use each character in text at most once.
 Return the maximum number of instances that can be formed."""
 
-class Solution(object):
-    def maxNumberOfBalloons(self, text):
-        """
-        :type text: str
-        :rtype: int
-        """
-        letters = {
-        'b': text.count('b'),
-        'a': text.count('a'),
-        'l': text.count('l') / 2,
-        'o': text.count('o') / 2,
-        'n': text.count('n')
-        }
+def maxNumberOfBalloons(self, text):
+    """
+    :type text: str
+    :rtype: int
+    """
+    letters = {
+    'b': text.count('b'),
+    'a': text.count('a'),
+    'l': text.count('l') / 2,
+    'o': text.count('o') / 2,
+    'n': text.count('n')
+    }
 
-        return min(letters.values())
+    return min(letters.values())
